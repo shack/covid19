ae96f59cee2ae4a83ae8f2ed8a4737cd9171ce5e
Sebastian Hack <hack@cs.uni-saarland.de>
2020-03-21 09:26:37 +0100

    comments

--- per_country.py
+++ per_country.py
@@ -3,9 +3,12 @@
 import argparse
 import urllib.request
 import csv
+import sys
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+
+from datetime import datetime
 from scipy.optimize import curve_fit
 
 data_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
@@ -34,16 +37,22 @@ parser.add_argument('-d', '--days', default=30, type=int, help='show only last N
 parser.add_argument('-f', '--fit',  default=5,  type=int, help='fit curve to last N days [default 5]')
 args = parser.parse_args()
 
+# load data
 content = map(lambda r : r.decode('utf-8'), urllib.request.urlopen(data_url))
 reader = csv.reader(content, delimiter=',', quotechar='|')
+
+# read the header row
 header = next(reader)
 
+# compute index of first day to plot, and index from where to fit
 idx_first_day = max(idx_first_day, len(header) - args.days)
-last_idx = -args.fit
+idx_fit       = -args.fit
 
-labels = header[idx_first_day:]
-xdata  = [i for i in range(0, len(labels))]
+# get the labels and sequential numbers for the x vals of the fitted function
+labels = [ datetime.strptime(s, '%m/%d/%y').strftime('%d-%m-%Y') for s in header[idx_first_day:] ]
+xdata  = [ i for i in range(0, len(labels)) ]
 
+# read data, filter countries and aggregate provinces
 for row in reader:
     cntry = row[1]
     if cntry in colors:
@@ -57,12 +66,13 @@ for row in reader:
 def func(x, k, b):
     return np.exp(k * x + b)
 
+# iterate over extracted data and fit functions
 for cntry, row in values.items():
     ydata = [ int(v) for v in row ]
     color, marker = colors[cntry]
     plt.plot(xdata, ydata, color=color, marker=marker, linestyle='dotted')
 
-    popt, pconv = curve_fit(func, xdata[last_idx:], ydata[last_idx:])
+    popt, pconv = curve_fit(func, xdata[idx_fit:], ydata[idx_fit:])
     yfit = [ func(x, popt[0], popt[1]) for x in xdata ]
     k = popt[0]
     b = popt[1]
